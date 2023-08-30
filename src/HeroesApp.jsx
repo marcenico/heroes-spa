b2b63e5089f24236d37c155b5a6331714a358196
Marcelo Puebla <marcenp23@gmail.com>
2023-08-30 08:43:18 -0300

    feat(routes): installed react router and created pages to navigate

--- src/HeroesApp.jsx
+++ src/HeroesApp.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
+import { AppRouter } from './router/AppRouter';
 
 export const HeroesApp = () => {
-  return <div>HeroesApp</div>;
+  return (
+    <>
+      <AppRouter></AppRouter>
+    </>
+  );
 };
