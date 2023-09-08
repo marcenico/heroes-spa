6e771aafcb2e8291426337018902615c591acb15
Marcelo Puebla <marcenp23@gmail.com>
2023-09-08 09:20:42 -0300

    feat(hero-page): added info to page

--- src/heroes/pages/HeroPage.jsx
+++ src/heroes/pages/HeroPage.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
+import { Navigate, useNavigate, useParams } from 'react-router-dom';
 import { getHeroById } from '../helpers';
 
 export const HeroPage = () => {
   const { id } = useParams();
   const hero = getHeroById(id);
+  const navigate = useNavigate();
+
+  const onNavigateBack = () => {
+    navigate(-1);
+  };
 
   if (!hero) return <Navigate to="/marvel" />;
 
   return (
-    <>
-      <h1>{hero.superhero}</h1>
-    </>
+    <div className="row mt-5">
+      <div className="col-4">
+        <img src={`/assets/heroes/${id}.jpg`} className="img-thumbnail " alt={hero.superhero} />
+      </div>
+      <div className="col-8">
+        <h3>{hero.superhero}</h3>
+
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">
+            <b>Alter ego:</b> {hero.alter_ego}
+          </li>
+          <li className="list-group-item">
+            <b>Publisher:</b> {hero.publisher}
+          </li>
+          <li className="list-group-item">
+            <b>First appearance:</b> {hero.first_appearance}
+          </li>
+        </ul>
+
+        <h5 className="mt-3">Characters</h5>
+        <p>{hero.characters}</p>
+
+        <button className="btn btn-outline-primary" onClick={() => onNavigateBack()}>
+          Back
+        </button>
+      </div>
+    </div>
   );
 };
