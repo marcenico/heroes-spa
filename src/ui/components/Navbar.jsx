54dfe94b49d3def5a9597f8fb5481c7beb272aa0
Marcelo Puebla <marcenp23@gmail.com>
2023-09-25 21:18:55 -0300

    feat(login): created basic login

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,7 +1,10 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth/context';
 
 export const Navbar = () => {
   const navigate = useNavigate();
+  const { user } = useContext(AuthContext);
 
   const onLogout = () => {
     navigate('/login', { replace: true });
@@ -31,7 +34,7 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary">Marcelo</span>
+          <span className="nav-item nav-link text-primary">{user?.name}</span>
 
           <button className="nav-item nav-link btn" type="button" onClick={onLogout}>
             Logout
