0d01b52554043f37d9fa25ff321ef2351f256da2
Marcelo Puebla <marcenp23@gmail.com>
2023-09-27 19:23:20 -0300

    feat(logout): added logout feature

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -7,17 +7,36 @@ const initialState = {
   logged: false
 };
 
+const init = () => {
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  return {
+    logged: !!user,
+    user: user
+  };
+};
+
 export const AuthProvider = ({ children }) => {
-  const [authState, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, initialState, init);
 
   const login = (name = '') => {
+    const user = { id: 'abc', name };
+
     const action = {
       type: types.login,
-      payload: { id: 'abc', name }
+      payload: user
     };
+
+    localStorage.setItem('user', JSON.stringify(user));
 
     dispatch(action);
   };
 
-  return <AuthContext.Provider value={{ ...authState, login: login }}>{children}</AuthContext.Provider>;
+  const logout = () => {
+    localStorage.removeItem('user');
+    const action = { type: types.logout };
+    dispatch(action);
+  };
+
+  return <AuthContext.Provider value={{ ...authState, login: login, logout: logout }}>{children}</AuthContext.Provider>;
 };
