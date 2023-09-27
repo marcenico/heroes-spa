328d77bf479422bcfd19dd17f68a2f31a8215d6c
Marcelo Puebla <marcenp23@gmail.com>
2023-09-27 19:32:43 -0300

    feat(router): added private routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
+import { PrivateRoute } from './PrivateRoute';
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
         <Route path="login" element={<LoginPage />} />
-        <Route path="/*" element={<HeroesRoutes />} />
+
+        <Route
+          path="/*"
+          element={
+            <PrivateRoute>
+              <HeroesRoutes />
+            </PrivateRoute>
+          }
+        />
       </Routes>
     </>
   );
