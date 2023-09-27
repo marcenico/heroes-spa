8a0ff3469b196de7d491f78908f72aadc1ffa94d
Marcelo Puebla <marcenp23@gmail.com>
2023-09-27 19:42:19 -0300

    feat(router): added public routes

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,12 +3,22 @@ import { Route, Routes } from 'react-router-dom';
 import { LoginPage } from '../auth';
 import { HeroesRoutes } from '../heroes';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
+        <Route
+          path="login/*"
+          element={
+            <PublicRoute>
+              <Routes>
+                <Route path="/*" element={<LoginPage />} />
+              </Routes>
+            </PublicRoute>
+          }
+        />
 
         <Route
           path="/*"
