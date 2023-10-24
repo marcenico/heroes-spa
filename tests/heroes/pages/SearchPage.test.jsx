5d497fff9313508628c406b69ae4f886ae08ca4f
Marcelo Puebla <marcenp23@gmail.com>
2023-10-23 22:55:00 -0300

    feat(search-page): added new unit test

--- tests/heroes/pages/SearchPage.test.jsx
+++ tests/heroes/pages/SearchPage.test.jsx
@@ -12,4 +12,22 @@ describe('Pruebas en <SearchPage />', () => {
 
     expect(container).toMatchSnapshot();
   });
+
+  test('debe de mostrar a batman y el input para buscar a batman', () => {
+    const queryValue = 'batman';
+
+    render(
+      <MemoryRouter initialEntries={[`/search?q=${queryValue}`]}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+
+    const input = screen.getByRole('textbox');
+    const img = screen.getByRole('img');
+    const alertDiv = screen.getByTestId('alert-danger');
+
+    expect(input.value).toBe(queryValue);
+    expect(img.src).toContain('/assets/heroes/dc-batman.jpg');
+    expect(alertDiv.style.display).toBe('none');
+  });
 });
